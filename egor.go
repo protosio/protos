4b023c5cd39b6342e671a9e829181738448d904f
Alex Giurgiu <alex.giurgiu@gmail.com>
2014-09-14 17:07:50 +0200

    option for starting and stoping containers

--- egor.go
+++ egor.go
@@ -4,14 +4,16 @@ import (
         //"database/sql"
         "fmt"
         //_ "github.com/mattn/go-sqlite3"
-        //"os"
-        "net/http"
-        "log"
+        "os"
+        "os/exec"
+        //"net/http"
+        //"log"
 	//"bytes"
-	"encoding/json"
-	"github.com/gorilla/mux"
-        "github.com/fsouza/go-dockerclient"
-	dkr "github.com/dotcloud/docker"
+	//"encoding/json"
+	//"github.com/gorilla/mux"
+        //"github.com/fsouza/go-dockerclient"
+	//dkr "github.com/dotcloud/docker"
+        "github.com/codegangsta/cli"
 )
 
 //func db() {
@@ -101,80 +103,104 @@ import (
 //        }
 //}
 //
-type User struct {
-	Email string
-	FirstName string
-}
-
-func GetUserHandler(w http.ResponseWriter, req *http.Request) {
-
-	user := User{ Email : "m...@me.com", FirstName: "Me"}
-
-	fmt.Println("GetUserHandler(): user: " + user.Email)
-	b, err := json.Marshal(user)
-	if err != nil {
-		fmt.Println(err)
-			w.WriteHeader(400)
-	} else {
-		w.Write(b)
-	}
-
- 	return 
-}
-
-func websrv() {
-
-    //r := mux.NewRouter()
-    //r.HandleFunc("/rest/user/", GetUserHandler).Methods("GET")
-    //r.Handle("/", http.FileServer(http.Dir("./")))
-
-    r := mux.NewRouter()
-    r.HandleFunc("/rest/user/", GetUserHandler).Methods("GET")
-    r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
-
-    // http.Handle("/rest/", r)
-    // http.Handle("/", http.FileServer(http.Dir("./")))
-
-    log.Fatal(http.ListenAndServe(":9009", r))
-}
-
-func test_docker() {
-        client, err := docker.NewClient("http://localhost:9196")
-        if err != nil {
-                log.Println("we haz error")
-                log.Fatal(err)
-        }	
-        fmt.Println(client)
-
-        id := "0b42a890ad91"
-        err = client.StartContainer(id, &dkr.HostConfig{})
-        if err != nil {
-                log.Fatal(err)
-        }
-       
-
-        opts := docker.ListContainersOptions{}
-
-        containers, err := client.ListContainers(opts)
-        if err != nil {
-                log.Fatal(err)
-        }
-        
-        fmt.Println(containers)
-
-        images, err := client.ListImages(true)
-        if err != nil {
-                log.Fatal(err)
-        }
-        fmt.Println(images)
-
-        
-}
+//type User struct {
+//	Email string
+//	FirstName string
+//}
+//
+//func GetUserHandler(w http.ResponseWriter, req *http.Request) {
+//
+//	user := User{ Email : "m...@me.com", FirstName: "Me"}
+//
+//	fmt.Println("GetUserHandler(): user: " + user.Email)
+//	b, err := json.Marshal(user)
+//	if err != nil {
+//		fmt.Println(err)
+//			w.WriteHeader(400)
+//	} else {
+//		w.Write(b)
+//	}
+//
+// 	return 
+//}
+//
+//func websrv() {
+//
+//    //r := mux.NewRouter()
+//    //r.HandleFunc("/rest/user/", GetUserHandler).Methods("GET")
+//    //r.Handle("/", http.FileServer(http.Dir("./")))
+//
+//    r := mux.NewRouter()
+//    r.HandleFunc("/rest/user/", GetUserHandler).Methods("GET")
+//    r.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+//
+//    // http.Handle("/rest/", r)
+//    // http.Handle("/", http.FileServer(http.Dir("./")))
+//
+//    log.Fatal(http.ListenAndServe(":9009", r))
+//}
+//
+//func test_docker() {
+//        client, err := docker.NewClient("http://localhost:9196")
+//        if err != nil {
+//                log.Println("we haz error")
+//                log.Fatal(err)
+//        }	
+//        fmt.Println(client)
+//
+//        id := "0b42a890ad91"
+//        err = client.StartContainer(id, &dkr.HostConfig{})
+//        if err != nil {
+//                log.Fatal(err)
+//        }
+//       
+//
+//        opts := docker.ListContainersOptions{}
+//
+//        containers, err := client.ListContainers(opts)
+//        if err != nil {
+//                log.Fatal(err)
+//        }
+//        
+//        fmt.Println(containers)
+//
+//        images, err := client.ListImages(true)
+//        if err != nil {
+//                log.Fatal(err)
+//        }
+//        fmt.Println(images)
+//
+//        
+//}
 
 func main() {
 	//db()
-        test_docker()
+        //test_docker()
         //websrv()
+
+  app := cli.NewApp()
+  app.Name = "egor"
+  app.Usage = "iz good for your privacy"
+  app.Action = func(c *cli.Context) {
+    println("I work!")
+  }
+
+  app.Commands = []cli.Command{
+  {
+    Name:      "start",
+    ShortName: "s",
+    Usage:     "starts an application",
+    Action: func(c *cli.Context) {
+      println("Running application: ", c.Args().First())
+      out, err := exec.Command("sh","-c",fmt.Sprintf("./images/%s/start.sh",c.Args().First())).Output()
+      if err != nil {
+        fmt.Printf("%s", err)
+      }
+      fmt.Printf("%s", out)
+    },
+  }}
+
+  app.Run(os.Args)
 }
 
 
