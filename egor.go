2fe03b22004c8ff20983241370a9488d7ec1bf8e
Alex Giurgiu <alex.giurgiu@gmail.com>
2014-11-17 19:04:29 +0100

    removed short name for commands and started implementing config loading and validation from yaml files

--- egor.go
+++ egor.go
@@ -4,16 +4,22 @@ import (
 	"fmt"
 	"github.com/codegangsta/cli"
 	"github.com/gorilla/mux"
-	//"gopkg.in/yaml.v2"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
 )
 
-//type AppConfig struct {
-//
-//}
+type AppConfig struct {
+	Description string
+	Ports       struct {
+		iport int
+		eport int
+	}
+}
 
 func webadmin(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello admin"))
@@ -33,6 +39,26 @@ func websrv() {
 
 }
 
+func validate_cfg(app string) {
+	log.Println("Validating config for", app)
+	filename, _ := filepath.Abs(fmt.Sprintf("./images/%s/app.yaml", app))
+	yamlFile, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var config AppConfig
+
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Value: %#v\n", config)
+	//fmt.Printf("Value: %#v\n", config.eport)
+}
+
 func main() {
 
 	app := cli.NewApp()
@@ -44,9 +70,8 @@ func main() {
 
 	app.Commands = []cli.Command{
 		{
-			Name:      "start",
-			ShortName: "s",
-			Usage:     "starts an application",
+			Name:  "start",
+			Usage: "starts an application",
 			Action: func(c *cli.Context) {
 				println("Running application: ", c.Args().First())
 				out, err := exec.Command("sh", "-c", fmt.Sprintf("./images/%s/start.sh", c.Args().First())).Output()
@@ -57,12 +82,18 @@ func main() {
 			},
 		},
 		{
-			Name:      "daemon",
-			ShortName: "d",
-			Usage:     "starts http server",
+			Name:  "daemon",
+			Usage: "starts http server",
 			Action: func(c *cli.Context) {
 				println("Starting webserver")
 				websrv()
+			},
+		},
+		{
+			Name:  "validate",
+			Usage: "validates application config",
+			Action: func(c *cli.Context) {
+				validate_cfg(c.Args().First())
 			},
 		},
 	}
