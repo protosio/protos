73d0f4223a61e150a90cd17f03eb7335e66ef3ea
Alex Giurgiu <alex.giurgiu@gmail.com>
2014-11-26 18:27:46 +0100

    moving towards using templates

--- daemon/web.go
+++ daemon/web.go
@@ -2,20 +2,35 @@ package daemon
 
 import (
 	"github.com/gorilla/mux"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+type Page struct {
+	Title string
+	Body  []byte
+}
+
 func Websrv() {
 	rtr := mux.NewRouter()
 
-	//rtr.HandleFunc("/admin", webadmin).Methods("GET")
-	//http.Handle("/", rtr)
+	fileHandler := http.StripPrefix("/static/", http.FileServer(http.Dir("./static")))
 
-	rtr.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
+	rtr.HandleFunc("/", MainHandler)
+	rtr.PathPrefix("/static").Handler(fileHandler)
 	http.Handle("/", rtr)
 
 	log.Println("Listening...")
 	http.ListenAndServe(":9000", nil)
+
+}
+
+func MainHandler(w http.ResponseWriter, r *http.Request) {
+
+	p := &Page{Title: "Egor dashboard", Body: []byte("page content")}
+
+	t := template.Must(template.ParseFiles("templates/index.html", "templates/header.html"))
+	t.Execute(w, p)
 
 }
