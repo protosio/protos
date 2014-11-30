fc7e197598de090d9285748ac895f6a7326209f1
Alex Giurgiu <alex.giurgiu@gmail.com>
2014-11-30 15:40:52 +0100

    moved the containerd/docker functionality in a new file

---
+++ daemon/app.go
@@ -0,0 +1,140 @@
+package daemon
+
+import (
+	"fmt"
+	"github.com/fsouza/go-dockerclient"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"path/filepath"
+)
+
+// Defines structure for config parameters
+// specific to each application
+type AppConfig struct {
+	Description string
+	Image       string
+	Ports       map[string]string
+	Data        string
+}
+
+type Config struct {
+	DataPath       string
+	AppsPath       string
+	Port           int
+	DockerEndpoint string
+	DockerClient   *docker.Client
+}
+
+var Gconfig Config
+
+func LoadAppCfg(app string) AppConfig {
+	log.Println("Reading config for [", app, "]")
+	filename, _ := filepath.Abs(fmt.Sprintf("%s/%s/app.yaml", Gconfig.AppsPath, app))
+	yamlFile, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var config AppConfig
+
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return config
+}
+
+func LoadCfg(config_file string) Config {
+	log.Println("Reading main config [", config_file, "]")
+	filename, _ := filepath.Abs(config_file)
+	yamlFile, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var config Config
+
+	err = yaml.Unmarshal(yamlFile, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	Gconfig = config
+
+	log.Println("Connecting to the docker daemon")
+	client, err := docker.NewClient(Gconfig.DockerEndpoint)
+	if err != nil {
+		log.Fatal(err)
+	}
+	Gconfig.DockerClient = client
+
+	return config
+}
+
+func StartApp(app string) {
+	log.Println("Starting application [", app, "]")
+	client := Gconfig.DockerClient
+
+	app_config := LoadAppCfg(app)
+
+	//Configure volumes
+	volumes := make(map[string]struct{})
+	var tmp struct{}
+	volumes[app_config.Data] = tmp
+
+	// Create container
+	config := docker.Config{Image: app_config.Image, Volumes: volumes}
+	create_options := docker.CreateContainerOptions{Name: "egor_" + app, Config: &config}
+	container, err := client.CreateContainer(create_options)
+	if err != nil {
+		log.Println("Could not create container")
+		log.Fatal(err)
+	}
+
+	// Configure ports
+	portsWrapper := make(map[docker.Port][]docker.PortBinding)
+	for key, value := range app_config.Ports {
+		ports := []docker.PortBinding{docker.PortBinding{HostIP: "0.0.0.0", HostPort: value}}
+		port_host := docker.Port(key)
+		portsWrapper[port_host] = ports
+	}
+
+	// Bind volumes
+	binds := []string{fmt.Sprintf("%s/%s:%s:rw", Gconfig.DataPath, app, app_config.Data)}
+
+	// Start container
+	host_config := docker.HostConfig{PortBindings: portsWrapper, Binds: binds}
+	err2 := client.StartContainer(container.ID, &host_config)
+	if err2 != nil {
+		log.Println("Could not start container")
+		log.Fatal(err2)
+	}
+
+}
+
+func StopApp(app string) {
+	log.Println("Stopping application [", app, "]")
+	client := Gconfig.DockerClient
+
+	err := client.StopContainer("egor_"+app, 3)
+	if err != nil {
+		log.Println("Could not stop application")
+		log.Fatal(err)
+	}
+
+	remove_options := docker.RemoveContainerOptions{ID: "egor_" + app}
+	err2 := client.RemoveContainer(remove_options)
+	if err2 != nil {
+		log.Fatal(err)
+	}
+}
+
+//func get_apps() []string {
+//	endpoint := "unix:///var/run/docker.sock"
+//	client, _ := docker.NewClient(endpoint)
+//
+//}
