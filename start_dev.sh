4e10fd15fb202d3f2a27a089bfc5fbf64555be35
Alex Giurgiu <alex.giurgiu@gmail.com>
2014-12-28 00:41:22 +0100

    changed dev path

--- start_dev.sh
+++ start_dev.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -ti -v /home/al3x/code/egor:/go/src/egor/ -v /var/run/docker.sock:/var/run/docker.sock golang:egor /bin/bash
+docker run -ti -v /home/al3x/code/egor_related/egor:/go/src/egor/ -v /var/run/docker.sock:/var/run/docker.sock golang:egor /bin/bash
