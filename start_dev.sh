a340e967dac119b376f77ae92cc9588d7bf85711
Alex Giurgiu <alex.giurgiu@gmail.com>
2015-06-25 17:41:03 +0200

    small modification to pass the ssh agent socket to the container

--- start_dev.sh
+++ start_dev.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker run -ti -v /home/al3x/code/egor_related/egor:/go/src/egor/ -v /var/run/docker.sock:/var/run/docker.sock golang:egor /bin/bash
+if [ -z "$SSH_AUTH_SOCK" ]; then
+    echo "SSH AGENT socket not set"
+else
+    SSH_VOLUME="-v $SSH_AUTH_SOCK:/tmp/ssh_agent -e SSH_AUTH_SOCK=/tmp/ssh_agent"
+fi
+
+docker run -ti -p 80:80 -v /egor:/go/src/egor/ $SSH_VOLUME -v /var/run/docker.sock:/var/run/docker.sock golang:egor /bin/bash
