5d8643763560cf338646104ed5445ecc77ac1cca
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-10-05 16:59:28 +0300

    added a new task scheduler, which is a goroutine that owns the tasks data. Methods retrieve data through that scheduler

---
+++ task/scheduler.go
@@ -0,0 +1,76 @@
+package task
+
+import (
+	"fmt"
+	"reflect"
+	"time"
+
+	"github.com/rs/xid"
+)
+
+// all ongoing tasks
+var tasks = make(map[string]Task)
+
+type readTaskResp struct {
+	tsk Task
+	err error
+}
+
+type readTaskReq struct {
+	id   string
+	resp chan readTaskResp
+}
+
+type requestTask struct {
+	t    Type
+	resp chan Task
+}
+
+// queue is a buffered channel to which we submit all the task requests
+var schedulerQueue = make(chan requestTask, 100)
+
+// readTaskQueue receives read requests for specific tasks, based on task id
+var readTaskQueue = make(chan readTaskReq)
+
+// readAllQueue receives read requests for the whole task list
+var readAllQueue = make(chan chan map[string]Task)
+
+func createTask(taskType Type) Task {
+	ts := time.Now()
+	tsk := Task{
+		ID:        xid.New().String(),
+		Status:    REQUESTED,
+		Progress:  &Progress{Percentage: 0},
+		StartedAt: &ts,
+		taskType:  taskType,
+
+		quitChan: make(chan bool),
+		getCopy:  make(chan Task),
+	}
+	log.WithField("proc", "scheduler").Debugf("Created new task %s with id %s", reflect.TypeOf(taskType), tsk.ID)
+	return tsk
+}
+
+// Scheduler takes care of scheduling long running background tasks
+func Scheduler(nrWorkers int) {
+	log.WithField("proc", "taskscheduler").Info("Starting the task scheduler")
+	for {
+		select {
+		case taskReq := <-schedulerQueue:
+			tsk := createTask(taskReq.t)
+			tasks[tsk.ID] = tsk
+			taskReq.resp <- tsk
+			log.WithField("proc", "scheduler").Infof("Running new task %s with id %s", reflect.TypeOf(taskReq.t), tsk.ID)
+			go tsk.Run()
+		case readReq := <-readTaskQueue:
+			if tsk, found := tasks[readReq.id]; found {
+				readReq.resp <- readTaskResp{tsk: tsk}
+			} else {
+				readReq.resp <- readTaskResp{err: fmt.Errorf("Could not find task %s", readReq.id)}
+			}
+		case readAllResp := <-readAllQueue:
+			readAllResp <- tasks
+		}
+	}
+
+}
--- task/task.go
+++ task/task.go
@@ -3,39 +3,76 @@ package task
 import (
 	"time"
 
-	"github.com/rs/xid"
+	"github.com/protosio/protos/util"
 )
 
+var log = util.GetLogger("task")
+
 const (
-	// REQUESTED - task state created
+	// REQUESTED - task has been created
 	REQUESTED = "REQUESTED"
+	// INPROGRESS - task is in progress
+	INPROGRESS = "INPROGRESS"
+	// ERROR - task has failed
+	ERROR = "ERROR"
+	// FINISHED - tash has been completed
+	FINISHED = "FINISHED"
 )
+
+// Type is the interface that all task types need to adhere too
+type Type interface {
+	Run() error
+}
+
+// Progress tracks the percentage and message of a task
+type Progress struct {
+	Percentage    int
+	StatusMessage string
+}
 
 // Task represents an (a)synchronous piece of work that Protos acts upon
 type Task struct {
-	ID         string    `json:"id"`
-	State      string    `json:"state"`
-	Resolution string    `json:"resolution"`
-	Progress   int       `json:"progress"`
-	StartedAt  time.Time `json:"started-at"`
-	FinishedAt time.Time `json:"finished-at"`
+	ID         string     `json:"id"`
+	Status     string     `json:"status"`
+	Progress   *Progress  `json:"progress"`
+	StartedAt  *time.Time `json:"started-at,omitempty"`
+	FinishedAt *time.Time `json:"finished-at,omitempty"`
+	taskType   Type
+
+	// Communication channels
+	quitChan chan bool
+	getCopy  chan Task
+}
+// GetCopy returns a copy of a running task
+func (t *Task) GetCopy() Task {
+	log.Debugf("Getting copy of task %s", t.ID)
+	t.getCopy <- Task{}
+	tsk := <-t.getCopy
+	return tsk
 }
 
-var tasks = make(map[string]*Task)
+//
+// General methods
+//
 
 // New creates a new task and returns it
-func New() (Task, error) {
-	task := Task{
-		ID:        xid.New().String(),
-		State:     REQUESTED,
-		Progress:  10,
-		StartedAt: time.Now(),
-	}
-	tasks[task.ID] = &task
-	return task, nil
+func New(taskType Type) (Task, error) {
+	rt := requestTask{t: taskType, resp: make(chan Task)}
+	schedulerQueue <- rt
+	return <-rt.resp, nil
 }
 
 // GetAll returns all the available tasks
-func GetAll() map[string]*Task {
-	return tasks
+func GetAll() map[string]Task {
+	resp := make(chan map[string]Task)
+	readAllQueue <- resp
+	return <-resp
+}
+
+// Get returns a task based on its id
+func Get(id string) (Task, error) {
+	rt := readTaskReq{id: id, resp: make(chan readTaskResp)}
+	readTaskQueue <- rt
+	resp := <-rt.resp
+	return resp.tsk, resp.err
 }
