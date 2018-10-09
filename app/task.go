033688aac6eb7e26f01124874895826893496cd9
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-10-09 15:20:43 +0300

    - starting the container once the Docker image has been downloaded - added a parameter to the app creation task which allows the caller to specify if the application should be started once its created. Will be useful for some situation like the init process

--- app/task.go
+++ app/task.go
@@ -1,8 +1,6 @@
 package app
 
 import (
-	"fmt"
-
 	"github.com/pkg/errors"
 	"github.com/protosio/protos/installer"
 	"github.com/protosio/protos/task"
@@ -14,6 +12,7 @@ type CreateAppTask struct {
 	InstallerVersion string
 	AppName          string
 	InstallerParams  map[string]string
+	StartOnCreation  bool
 }
 
 // Run starts the async task
@@ -29,14 +28,13 @@ func (t CreateAppTask) Run(pt *task.Task) error {
 
 	metadata, err := inst.ReadVersion(t.InstallerVersion)
 	if err != nil {
-		return fmt.Errorf("Could not create application %s: %s", t.AppName, err.Error())
+		return errors.Wrapf(err, "Could not create application %s", t.AppName)
 	}
 
 	app, err := Create(t.InstallerID, t.InstallerVersion, t.AppName, t.InstallerParams, metadata)
 	if err != nil {
-		return fmt.Errorf("Could not create application %s: %s", t.AppName, err.Error())
+		return errors.Wrapf(err, "Could not create application %s", t.AppName)
 	}
-	defer app.Remove()
 	pt.Progress.Percentage = 10
 	pt.Progress.StatusMessage = "Created application"
 	pt.Update()
@@ -45,10 +43,24 @@ func (t CreateAppTask) Run(pt *task.Task) error {
 		log.WithField("proc", pt.ID).Debugf("Docker image %s for installer %s(%s) is not available locally. Downloading...", metadata.PlatformID, t.InstallerID, t.InstallerVersion)
 		err = inst.Download(pt, t.InstallerVersion)
 		if err != nil {
+			app.Remove()
 			return errors.Wrapf(err, "Could not create application %s", t.AppName)
 		}
 	} else {
 		log.WithField("proc", pt.ID).Debugf("Docker image for installer %s(%s) found locally", t.InstallerID, t.InstallerVersion)
+	}
+
+	err = app.createContainer()
+	if err != nil {
+		app.Remove()
+		return errors.Wrapf(err, "Could not create application %s", t.AppName)
+	}
+
+	if t.StartOnCreation {
+		err = app.Start()
+		if err != nil {
+			return errors.Wrapf(err, "Could not create application %s", t.AppName)
+		}
 	}
 
 	return nil
