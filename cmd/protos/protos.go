94bbe08301c328727fc7f3c5fe1eaa046d3a02c0
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-10-20 13:14:31 +0300

    moved all init code to the daemon package, makes more sense.

--- cmd/protos/protos.go
+++ cmd/protos/protos.go
@@ -2,91 +2,13 @@ package main
 
 import (
 	"os"
-	"sync"
 
-	"github.com/protosio/protos/task"
-
-	"github.com/protosio/protos/app"
-
-	"github.com/protosio/protos/platform"
-	"github.com/protosio/protos/provider"
-	"github.com/protosio/protos/resource"
-
-	"github.com/protosio/protos/api"
-	"github.com/protosio/protos/auth"
-	"github.com/protosio/protos/capability"
-	"github.com/protosio/protos/config"
 	"github.com/protosio/protos/daemon"
-	"github.com/protosio/protos/database"
-	"github.com/protosio/protos/meta"
 	"github.com/protosio/protos/util"
 
 	"github.com/sirupsen/logrus"
 	cli "gopkg.in/urfave/cli.v1"
 )
-
-func run(configFile string) {
-	var wg sync.WaitGroup
-	config.Load(configFile)
-	log := util.GetLogger("cmd")
-
-	if database.Exists() {
-		database.Open()
-		defer database.Close()
-		capability.Initialize()
-		platform.Initialize()      // required to connect to the Docker daemon
-		app.LoadAppsDB()           // required to register the application structs with the DB
-		resource.LoadResourcesDB() // required to register the resource structs with the DB
-		provider.LoadProvidersDB() // required to register the provider structs with the DB
-		cert := meta.Initialize()
-
-		daemon.StartUp()
-		wg.Add(3)
-		go func() {
-			app.Manager()
-			wg.Done()
-		}()
-		go func() {
-			task.Scheduler(20)
-			wg.Done()
-		}()
-		go func() {
-			api.Websrv(cert)
-			wg.Done()
-		}()
-		wg.Wait()
-	} else {
-		log.Info("Database file doesn't exists. Running in web init mode")
-		database.Open()
-		defer database.Close()
-		capability.Initialize()
-
-		meta.Setup()
-		meta.SetPublicIP()
-
-		platform.Initialize()      // required to connect to the Docker daemon
-		app.LoadAppsDB()           // required to register the application structs with the DB
-		resource.LoadResourcesDB() // required to register the resource structs with the DB
-		provider.LoadProvidersDB() // required to register the provider structs with the DB
-		daemon.StartUp()
-		wg.Add(3)
-		go func() {
-			app.Manager()
-			wg.Done()
-		}()
-		go func() {
-			task.Scheduler(20)
-			wg.Done()
-		}()
-		go func() {
-			api.Websrv(nil)
-			wg.Done()
-		}()
-		wg.Wait()
-
-	}
-
-}
 
 func main() {
 
@@ -128,7 +50,7 @@ func main() {
 			Name:  "daemon",
 			Usage: "start the server",
 			Action: func(c *cli.Context) error {
-				run(configFile)
+				daemon.StartUp(configFile, false)
 				return nil
 			},
 		},
@@ -136,12 +58,7 @@ func main() {
 			Name:  "init",
 			Usage: "create initial configuration and user",
 			Action: func(c *cli.Context) error {
-				config.Load(configFile)
-				daemon.Setup()
-				database.Open()
-				defer database.Close()
-				meta.Setup()
-				auth.SetupAdmin()
+				daemon.StartUp(configFile, true)
 				return nil
 			},
 		},
--- daemon/daemon.go
+++ daemon/daemon.go
@@ -2,18 +2,39 @@ package daemon
 
 import (
 	"os"
+	"sync"
 
+	"github.com/protosio/protos/api"
+	"github.com/protosio/protos/app"
+	"github.com/protosio/protos/provider"
+	"github.com/protosio/protos/resource"
+	"github.com/protosio/protos/task"
+
+	"github.com/protosio/protos/capability"
 	"github.com/protosio/protos/config"
+	"github.com/protosio/protos/database"
+	"github.com/protosio/protos/meta"
+	"github.com/protosio/protos/platform"
 	"github.com/protosio/protos/util"
 )
 
 var gconfig = config.Get()
 var log = util.GetLogger("daemon")
 
+func run(wg *sync.WaitGroup, manager func(chan bool), quit chan bool) {
+	go func() {
+		manager(quit)
+		wg.Done()
+	}()
+}
+
 // StartUp triggers a sequence of steps required to start the application
-func StartUp() {
+func StartUp(configFile string, init bool) {
 	log.Info("Starting up...")
 	var err error
+	var wg sync.WaitGroup
+	config.Load(configFile)
+	gconfig.InitMode = (database.Exists() == false) || init
 
 	// Generate secret key used for JWT
 	log.Info("Generating secret for JWT")
@@ -21,6 +42,43 @@ func StartUp() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// open databse
+	database.Open()
+	defer database.Close()
+
+	// If db does not exist or init is set to true, need to run in init mode and create the db
+	if gconfig.InitMode {
+		log.Info("Database file doesn't exists or init mode requested. Running in web init mode")
+		meta.Setup()
+	}
+
+	capability.Initialize()
+	platform.Initialize()      // required to connect to the Docker daemon
+	resource.LoadResourcesDB() // required to register the resource structs with the DB
+	provider.LoadProvidersDB() // required to register the provider structs with the DB
+
+	wg.Add(3)
+	// start app manager
+	gconfig.ProcsQuit["appmanager"] = make(chan bool)
+	run(&wg, app.Manager, gconfig.ProcsQuit["appmanager"])
+	// start task scheduler
+	gconfig.ProcsQuit["taskscheduler"] = make(chan bool)
+	run(&wg, task.Scheduler, gconfig.ProcsQuit["taskscheduler"])
+
+	if gconfig.InitMode {
+		// run the init webserver in blocking mode
+		gconfig.ProcsQuit["initwebserver"] = make(chan bool)
+		api.WebsrvInit(gconfig.ProcsQuit["initwebserver"])
+		log.Info("Finished initialisation. Resuming normal operations")
+	}
+	gconfig.InitMode = false
+
+	meta.InitCheck()
+	// start tls web server
+	gconfig.ProcsQuit["webserver"] = make(chan bool)
+	run(&wg, api.Websrv, gconfig.ProcsQuit["webserver"])
+	wg.Wait()
 
 }
 
