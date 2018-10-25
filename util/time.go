1248dbf6b03699b310c60b2bd6dd370da737a9cc
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-10-25 15:18:22 +0300

    added custom Gob encoder and decoder for ProtosTime. It's needed for storing the Task in the database

--- util/time.go
+++ util/time.go
@@ -9,8 +9,25 @@ import (
 type ProtosTime time.Time
 
 // MarshalJSON is a customer JSON marshallers for the ProtosTime
-func (pt ProtosTime) MarshalJSON() ([]byte, error) {
-	t := time.Time(pt)
+func (pt *ProtosTime) MarshalJSON() ([]byte, error) {
+	t := time.Time(*pt)
 	stamp := fmt.Sprintf("\"%s\"", t.Format(time.StampMilli))
 	return []byte(stamp), nil
+}
+
+// GobEncode is a custom gob encoder for ProtosTime. Gob is used as a serialisation format to store stuff in the db
+func (pt *ProtosTime) GobEncode() ([]byte, error) {
+	t := time.Time(*pt)
+	return t.MarshalBinary()
+}
+
+// GobDecode is a custom gob decoder for ProtosTime. Gob is used as a serialisation format to store stuff in the db
+func (pt *ProtosTime) GobDecode(buf []byte) error {
+	t := time.Time(*pt)
+	err := t.UnmarshalBinary(buf)
+	if err != nil {
+		return err
+	}
+	*pt = ProtosTime(t)
+	return nil
 }
