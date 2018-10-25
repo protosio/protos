d5c7acb7123edda91d3c15a8d9200f48cc102cab
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-10-25 15:21:13 +0300

    Tasks are now saved in the database

--- task/scheduler.go
+++ task/scheduler.go
@@ -1,16 +1,19 @@
 package task
 
 import (
+	"encoding/gob"
 	"fmt"
 	"reflect"
 	"time"
 
+	"github.com/pkg/errors"
+	"github.com/protosio/protos/database"
 	"github.com/protosio/protos/util"
 	"github.com/rs/xid"
 )
 
 // all ongoing tasks
-var tasks = make(map[string]Task)
+var tasks map[string]Task
 
 type readTaskResp struct {
 	tsk Task
@@ -39,35 +42,64 @@ var updateTaskQueue = make(chan Task, 1000)
 // readAllQueue receives read requests for the whole task list
 var readAllQueue = make(chan chan map[string]Task)
 
+func initDB() map[string]Task {
+	log.WithField("proc", "taskscheduler").Debug("Retrieving tasks from DB")
+	gob.Register(&Task{})
+	gob.Register(&util.ProtosTime{})
+
+	dbtasks := []Task{}
+	err := database.All(&dbtasks)
+	if err != nil {
+		log.Fatal("Could not retrieve tasks from database: ", err)
+	}
+
+	ltasks := make(map[string]Task)
+	for _, task := range dbtasks {
+		ltasks[task.ID] = task
+	}
+	return ltasks
+}
+
+func saveTask(task Task) {
+	log.WithField("proc", "taskscheduler").Debugf("Saving task %s to database", task.ID)
+	err := database.Save(&task)
+	if err != nil {
+		log.Panic(errors.Wrapf(err, "Could not save task %s to database", task.ID))
+	}
+}
+
 func createTask(taskType Type) Task {
 	ts := util.ProtosTime(time.Now())
 	tsk := Task{
 		ID:        xid.New().String(),
 		Status:    REQUESTED,
-		Progress:  &Progress{Percentage: 0},
+		Progress:  Progress{Percentage: 0},
 		StartedAt: &ts,
 		taskType:  taskType,
 
 		quitChan: make(chan bool),
 	}
-	log.WithField("proc", "scheduler").Debugf("Created new task %s with id %s", reflect.TypeOf(taskType), tsk.ID)
+	saveTask(tsk)
+	log.WithField("proc", "taskscheduler").Debugf("Created new task %s with id %s", reflect.TypeOf(taskType), tsk.ID)
 	return tsk
 }
 
 // Scheduler takes care of scheduling long running background tasks
 func Scheduler(quit chan bool) {
 	log.WithField("proc", "taskscheduler").Info("Starting the task scheduler")
+	tasks = initDB()
 	for {
 		select {
 		case taskReq := <-schedulerQueue:
 			tsk := createTask(taskReq.t)
 			tasks[tsk.ID] = tsk
 			taskReq.resp <- tsk
-			log.WithField("proc", "scheduler").Infof("Running new task %s with id %s", reflect.TypeOf(taskReq.t), tsk.ID)
+			log.WithField("proc", "taskscheduler").Infof("Running new task %s with id %s", reflect.TypeOf(taskReq.t), tsk.ID)
 			go tsk.Run()
 		case tsk := <-updateTaskQueue:
-			log.WithField("proc", "scheduler").Debugf("Updating task %s", tsk.ID)
+			log.WithField("proc", "taskscheduler").Debugf("Updating task %s", tsk.ID)
 			tasks[tsk.ID] = tsk
+			saveTask(tsk)
 		case readReq := <-readTaskQueue:
 			if tsk, found := tasks[readReq.id]; found {
 				readReq.resp <- readTaskResp{tsk: tsk}
