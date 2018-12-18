8731556e8ad3d4c11fe4bec8588054c56038b8f5
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2018-12-18 18:23:49 +0200

    simplified the CPU information

--- platform/stats.go
+++ platform/stats.go
@@ -1,15 +1,25 @@
 package platform
 
 import (
+	"time"
+
 	"github.com/shirou/gopsutil/cpu"
 	"github.com/shirou/gopsutil/disk"
 	"github.com/shirou/gopsutil/mem"
 )
 
+// CPUInfo holds information about the characteristics of the CPU
+type CPUInfo struct {
+	Model     string  `json:"model"`
+	Cores     int     `json:"cores"`
+	Frequency float64 `json:"frequency"`
+	Cache     int32   `json:"cache"`
+}
+
 // CPUStats holds information about the characteristics of the CPU and it's usage
 type CPUStats struct {
-	Usage []cpu.TimesStat `json:"usage"`
-	Info  []cpu.InfoStat  `json:"info"`
+	Usage int     `json:"usage"`
+	Info  CPUInfo `json:"info"`
 }
 
 // HardwareStats holds information about the state and usage of the system
@@ -22,19 +32,28 @@ type HardwareStats struct {
 // GetHWStats returns the current system stats
 func GetHWStats() (HardwareStats, error) {
 	hw := HardwareStats{}
+
 	memStat, err := mem.VirtualMemory()
 	if err != nil {
 		return hw, err
 	}
-	cpuInfo, err := cpu.Info()
+
+	cpuDetailedInfo, err := cpu.Info()
 	if err != nil {
 		return hw, err
 	}
-	cpuStats, err := cpu.Times(false)
+	cpuInfo := CPUInfo{
+		Model:     cpuDetailedInfo[0].ModelName,
+		Cores:     len(cpuDetailedInfo),
+		Frequency: cpuDetailedInfo[0].Mhz,
+		Cache:     cpuDetailedInfo[0].CacheSize,
+	}
+	cpuUsage, err := cpu.Percent(time.Second, false)
 	if err != nil {
 		return hw, err
 	}
-	cpuStat := CPUStats{Info: cpuInfo, Usage: cpuStats}
+	cpuStat := CPUStats{Info: cpuInfo, Usage: int(cpuUsage[0])}
+
 	diskStat, err := disk.Usage("/")
 	if err != nil {
 		return hw, err
