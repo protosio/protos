ae083aefaaf9510a51b09a912cf9ad9781ff0112
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2019-04-19 20:28:57 +0300

    task related interfaces

---
+++ core/task.go
@@ -0,0 +1,35 @@
+package core
+
+import "github.com/emirpasic/gods/maps/linkedhashmap"
+
+type TaskManager interface {
+	New(CustomTask) Task
+	GetAll() *linkedhashmap.Map
+	GetIDs([]string) linkedhashmap.Map
+}
+
+// CustomTask is the interface that is implemented by custom tasks in various packages
+type CustomTask interface {
+	Run(string, Progress) error
+	Name() string
+}
+
+type Progress interface {
+	SetPercentage(int)
+	SetState(string)
+}
+
+// Task is the interface that all task types need to adhere too
+type Task interface {
+	GetID() string
+	Wait() error
+	SetPercentage(int)
+	GetPercentage() int
+	SetState(string)
+	SetStatus(string)
+	AddApp(string)
+	Copy() Task
+	SetKillable()
+	Dying() <-chan struct{}
+	Save()
+}
