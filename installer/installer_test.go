80c27e2d0b72f7ba48c4dacc9b8764e6207ef07f
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2019-06-21 23:21:37 +0200

    added some tests for the installer metadata related code

--- installer/installer_test.go
+++ installer/installer_test.go
@@ -8,6 +8,7 @@ import (
 )
 
 func TestInstaller(t *testing.T) {
+
 	caps := parseInstallerCapabilities("ResourceProvider,WrongCap")
 	if len(caps) != 1 {
 		t.Errorf("Wrong number of capabilities returned. %d instead of 1", len(caps))
@@ -22,6 +23,38 @@ func TestInstaller(t *testing.T) {
 	}
 	if ports[0].Nr != 1 || ports[0].Type != util.TCP || ports[1].Nr != 2 || ports[1].Type != util.UDP {
 		t.Errorf("Wrong data in the ports array returned by the parsePublicPorts: %v", ports)
+	}
+
+	//
+	// installer metadata tests
+	//
+
+	testMetadata := map[string]string{
+		"protos.installer.metadata.capabilities": "ResourceProvider,ResourceConsumer,InternetAccess,GetInformation,PublicDNS,AuthUser",
+		"protos.installer.metadata.requires":     "dns",
+		"protos.installer.metadata.provides":     "mail",
+		"protos.installer.metadata.publicports":  "80/tcp,443/tcp,9999/udp",
+		"protos.installer.metadata.name":         "testapp",
+	}
+
+	_, err := GetMetadata(testMetadata)
+	if err == nil {
+		t.Errorf("GetMetadata(testMetadata) should return an error on missing description")
+	}
+
+	testMetadata["protos.installer.metadata.description"] = "Small app description"
+
+	metadata, err := GetMetadata(testMetadata)
+	if err != nil {
+		t.Errorf("GetMetadata(testMetadata) should not return an error, but it did: %s", err)
+	}
+
+	if len(metadata.PublicPorts) != 3 {
+		t.Errorf("There should be %d publicports in the metadata. There are %d", 3, len(metadata.PublicPorts))
+	}
+
+	if (len(metadata.Requires) == 1 && metadata.Requires[0] != "dns") || len(metadata.Requires) != 1 {
+		t.Errorf("metadata.Requires should only have 'dns' stored: %v", metadata.Requires)
 	}
 
 }
