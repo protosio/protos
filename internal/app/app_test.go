4760255611012c960b3c5622532fa316f025ba25
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2019-09-18 15:18:01 +0300

    added test for app.GetServices method

--- internal/app/app_test.go
+++ internal/app/app_test.go
@@ -8,6 +8,7 @@ import (
 	"protos/internal/capability"
 	"protos/internal/core"
 	"protos/internal/mock"
+	"protos/internal/util"
 
 	"github.com/emirpasic/gods/maps/linkedhashmap"
 	"github.com/golang/mock/gomock"
@@ -30,7 +31,7 @@ func TestAppManager(t *testing.T) {
 		Do(func(to interface{}) {
 			apps := to.(*[]*App)
 			*apps = append(*apps,
-				&App{ID: "id1", Name: "app1", access: &sync.Mutex{}},
+				&App{ID: "id1", Name: "app1", access: &sync.Mutex{}, PublicPorts: []util.Port{util.Port{Nr: 10000, Type: util.TCP}}},
 				&App{ID: "id2", Name: "app2", access: &sync.Mutex{}},
 				&App{ID: "id3", Name: "app3", access: &sync.Mutex{}})
 		})
@@ -125,7 +126,7 @@ func TestAppManager(t *testing.T) {
 	rmMock.EXPECT().CreateDNS(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).Return(nil, fmt.Errorf("test error")).Times(1)
 	_, err = appMgr.Create("a", "b", "c", map[string]string{}, core.InstallerMetadata{Capabilities: []*capability.Capability{capability.PublicDNS}}, "taskid")
 	if err == nil {
-		t.Errorf("Creating an app and having a DNS creation error should result in an error")
+		t.Error("Creating an app and having a DNS creation error should result in an error")
 	}
 
 	// happy case
@@ -135,4 +136,30 @@ func TestAppManager(t *testing.T) {
 	rmMock.EXPECT().Select(gomock.Any()).Return(map[string]core.Resource{}).Times(1)
 	dbMock.EXPECT().Save(gomock.Any()).Return(nil).Times(1)
 	_, err = appMgr.Create("a", "b", "c", map[string]string{}, core.InstallerMetadata{}, "taskid")
+
+	//
+	// GetServices
+	//
+
+	dnsRscType := mock.NewMockType(ctrl)
+	dnsRscType.EXPECT().GetName().Return("app1").Times(2)
+	dnsRscType.EXPECT().GetValue().Return("1.1.1.1").Times(1)
+	rscMock := mock.NewMockResource(ctrl)
+	rscMock.EXPECT().GetAppID().Return("id1").Times(1)
+	rscMock.EXPECT().GetValue().Return(dnsRscType).Times(1)
+	rmMock.EXPECT().Select(gomock.Any()).Return(map[string]core.Resource{"1": rscMock}).Times(1)
+	metaMock.EXPECT().GetDomain().Return("giurgiu.io").Times(1)
+	services := am.GetServices()
+	if len(services) != 1 {
+		t.Fatalf("GetServices should only return 1 service in this test, but %d were returned", len(services))
+	}
+	svc := services[0]
+	if len(svc.Ports) != 1 ||
+		svc.Ports[0].Nr != 10000 ||
+		svc.Ports[0].Type != util.TCP ||
+		svc.Name != "app1" ||
+		svc.Domain != "app1.giurgiu.io" {
+		t.Error("GetServices returned a service with incorrect values")
+	}
+
 }
