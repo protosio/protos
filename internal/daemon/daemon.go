019f7341ec913247a1272fa1ce7bfec61483ada1
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2019-10-30 16:39:40 +0200

    removed unused method

--- internal/daemon/daemon.go
+++ internal/daemon/daemon.go
@@ -114,21 +114,3 @@ func StartUp(configFile string, init bool, version *semver.Version, incontainer 
 	log.Info("Terminating...")
 
 }
-
-// Setup creates the Protos work directory
-func Setup() {
-
-	// create the workdir if it does not exist
-	if _, err := os.Stat(gconfig.WorkDir); err != nil {
-		if os.IsNotExist(err) {
-			log.Info("Creating working directory [", gconfig.WorkDir, "]")
-			err = os.Mkdir(gconfig.WorkDir, 0755)
-			if err != nil {
-				log.Fatal(err)
-			}
-		} else {
-			log.Fatal(err)
-		}
-	}
-
-}
