9bf39b43944f56a99a9d9439fad55233c5fd82bd
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2020-03-19 00:07:06 +0200

    since we are starting the https server later, the method capabilities get applied again so we can't have a panic because of it. Need to improve the whole capability system

--- internal/capability/capability.go
+++ internal/capability/capability.go
@@ -66,9 +66,6 @@ func (cm *Manager) Validate(methodcap core.Capability, appcap string) bool {
 // SetMethodCap adds a capability for a specific method
 func (cm *Manager) SetMethodCap(method string, cap core.Capability) {
 	lcap := cap.(*Capability)
-	if _, err := cm.GetMethodCap(method); err == nil {
-		log.Panicf("Method '%s' already has a capability set", method)
-	}
 	log.Debugf("Setting capability '%s' for method '%s'", lcap.Name, method)
 	cm.capMap[method] = lcap
 }
