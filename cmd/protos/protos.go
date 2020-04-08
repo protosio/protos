160f923d625ef65cd2645b451e7d40df50ea46d0
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2020-04-08 13:23:23 +0300

    proper version

--- cmd/protos/protos.go
+++ cmd/protos/protos.go
@@ -17,7 +17,7 @@ func main() {
 	app.Name = "protosd"
 	app.Author = "Alex Giurgiu"
 	app.Email = "alex@giurgiu.io"
-	version, err := semver.NewVersion("0.0.0-testimg")
+	version, err := semver.NewVersion("0.0.0-dev.4")
 	if err != nil {
 		panic(err)
 	}
