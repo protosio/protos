1ab33007d9b39e42f7f765c56342f3f358723852
Alexandru Giurgiu <alex.giurgiu@gmail.com>
2020-05-11 20:49:30 +0300

    new db DBCLI interface. Will swap it with the old interface once all code uses the new one

--- internal/core/db.go
+++ internal/core/db.go
@@ -11,3 +11,13 @@ type DB interface {
 	Open()
 	Close()
 }
+
+// DBCLI represents a DB client instance, used to interract with the database
+type DBCLI interface {
+	SaveStruct(dataset string, data interface{}) error
+	GetStruct(dataset string, to interface{}) error
+	GetSet(dataset string, to interface{}) error
+	InsertInSet(dataset string, data interface{}) error
+	RemoveFromSet(dataset string, data interface{}) error
+	Close() error
+}
