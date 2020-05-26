4362396bde9f55c5cd3932319ae81c3ce15882dd
Alex Giurgiu <alex@giurgiu.io>
2020-05-26 21:59:54 +0300

    updated build scripts and files

---
+++ build/build_linuxkit.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+mkdir -p ${SCRIPTPATH}/src
+function finish {
+   rm -rf ${SCRIPTPATH}/src
+}
+trap finish EXIT
+
+cp -R ${SCRIPTPATH}/../{cmd,configs,internal,pkg,go.mod,go.sum} ${SCRIPTPATH}/src/
+linuxkit pkg build -disable-cache $SCRIPTPATH
+
