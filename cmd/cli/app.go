ccd1bd2418d72363901acd81322dabbac334cda4
Alex Giurgiu <alex@giurgiu.io>
2020-06-01 21:47:26 +0300

    added subcommand for displaying extended info about an app from the store

--- cmd/cli/app.go
+++ cmd/cli/app.go
@@ -4,6 +4,7 @@ import (
 	// "github.com/protosio/cli/internal/app"
 	"fmt"
 	"os"
+	"strings"
 	"text/tabwriter"
 
 	"github.com/urfave/cli/v2"
@@ -36,6 +37,19 @@ var cmdApp *cli.Command = &cli.Command{
 					Usage: "List all applications in the store",
 					Action: func(c *cli.Context) error {
 						return listAppStoreApps()
+					},
+				},
+				{
+					Name:      "info",
+					ArgsUsage: "<id>",
+					Usage:     "Display extended information about an app from the store",
+					Action: func(c *cli.Context) error {
+						id := c.Args().Get(0)
+						if id == "" {
+							cli.ShowSubcommandHelp(c)
+							os.Exit(1)
+						}
+						return infoAppStoreApp(id)
 					},
 				},
 			},
@@ -71,5 +85,33 @@ func listAppStoreApps() error {
 		fmt.Fprintf(w, "\n %s\t%s\t%s\t%s\t", installer.GetName(), id, installer.GetLastVersion(), instMetadata.Description)
 	}
 	fmt.Fprint(w, "\n")
+	return nil
+}
+
+func infoAppStoreApp(id string) error {
+	installer, err := envi.AS.GetInstaller(id)
+	if err != nil {
+		return err
+	}
+
+	instMetadata, err := installer.GetMetadata(installer.GetLastVersion())
+	if err != nil {
+		return err
+	}
+
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 0, 0, 2, ' ', 0)
+
+	defer w.Flush()
+
+	fmt.Fprintf(w, "%s\t%s\t", "Name:", installer.GetName())
+	fmt.Fprintf(w, "\n%s\t%s\t", "ID:", id)
+	fmt.Fprintf(w, "\n%s\t%s\t", "Description:", instMetadata.Description)
+	fmt.Fprintf(w, "\n%s\t%s\t", "Version: ", installer.GetLastVersion())
+	fmt.Fprintf(w, "\n%s\t%s\t", "Requires resources: ", strings.Join(instMetadata.Requires, ","))
+	fmt.Fprintf(w, "\n%s\t%s\t", "Provides resources: ", strings.Join(instMetadata.Provides, ","))
+	fmt.Fprintf(w, "\n%s\t%s\t", "Capabilities: ", strings.Join(instMetadata.Capabilities, ","))
+	fmt.Fprint(w, "\n")
+
 	return nil
 }
