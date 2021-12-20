17703ef4abfc60accfe59eb8b2898dc2490438a6
Alex Giurgiu <alex@giurgiu.io>
2021-12-20 23:34:02 +0200

    we should not return an error if the DNS file already exists. We just overwrite it. That way, the VPN start operation is idempotent

--- internal/vpn/dns_darwin.go
+++ internal/vpn/dns_darwin.go
@@ -17,21 +17,16 @@ type dnsManager struct {
 
 func (m *dnsManager) AddDomainServer(domain string, server net.IP) error {
 	if domain == "" {
-		return fmt.Errorf("Domain cannot be empty")
+		return fmt.Errorf("domain cannot be empty")
 	}
 
 	// check if the file exists
 	resolverFile := resolverPath + "/" + domain
-	_, err := os.Stat(resolverFile)
-	if err == nil {
-		return fmt.Errorf("Could not add DNS server for domain '%s': file '%s' already exists", domain, resolverFile)
-	}
-
 	// write file
 	dnsData := fmt.Sprintf("nameserver %s\n", server.String())
-	err = ioutil.WriteFile(resolverFile, []byte(dnsData), 0644)
+	err := ioutil.WriteFile(resolverFile, []byte(dnsData), 0644)
 	if err != nil {
-		return fmt.Errorf("Could not add DNS server for domain '%s': %w", domain, err)
+		return fmt.Errorf("could not add DNS server for domainss '%s': %w", domain, err)
 	}
 
 	return nil
@@ -39,14 +34,14 @@ func (m *dnsManager) AddDomainServer(domain string, server net.IP) error {
 
 func (m *dnsManager) DelDomainServer(domain string) error {
 	if domain == "" {
-		return fmt.Errorf("Domain cannot be empty")
+		return fmt.Errorf("domain cannot be empty")
 	}
 
 	// check if the file exists
 	resolverFile := resolverPath + "/" + domain
 	err := os.Remove(resolverFile)
 	if err != nil {
-		return fmt.Errorf("Could not delete DNS server for domain '%s': %w", domain, err)
+		return fmt.Errorf("could not delete DNS server for domain '%s': %w", domain, err)
 	}
 
 	return nil
