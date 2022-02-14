6a3437ec9bb4024025245cee0e4fb7897a976f81
Alex Giurgiu <alex@giurgiu.io>
2022-02-14 15:30:03 +0200

    fixed linuxkit build script

--- scripts/build_linuxkit.sh
+++ scripts/build_linuxkit.sh
@@ -3,5 +3,5 @@
 set -e
 
 SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-linuxkit pkg build -docker -build-yml $SCRIPTPATH/../linuxkit_build.yaml  $SCRIPTPATH/..
+linuxkit pkg build -docker -build-yml linuxkit_build.yaml  $SCRIPTPATH/..
 
